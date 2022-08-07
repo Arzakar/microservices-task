e8bc9d4b989956b00dad5f6e1cb4cc15a23d38f3
Stepan Klimashin <stepan_klimashin@epam.com>
2022-08-07 20:49:37 +0400

    Implement docker-compose for other business services. Settings zipkin and graphite

--- business-services/one/src/main/java/com/rntgroup/controller/SimpleOneController.java
+++ business-services/one/src/main/java/com/rntgroup/controller/SimpleOneController.java
@@ -1,5 +1,6 @@
 package com.rntgroup.controller;
 
+import com.netflix.servo.DefaultMonitorRegistry;
 import com.netflix.servo.annotations.DataSourceType;
 import com.netflix.servo.annotations.Monitor;
 import com.netflix.servo.annotations.MonitorTags;
@@ -9,27 +10,35 @@ import com.netflix.servo.monitor.Monitors;
 import com.netflix.servo.monitor.Stopwatch;
 import com.netflix.servo.tag.BasicTagList;
 import com.netflix.servo.tag.TagList;
+import com.rntgroup.client.SimpleTwoClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 @RestController
 public class SimpleOneController {
 
+    @Autowired
+    private SimpleTwoClient simpleTwoClient;
+
     @Monitor(name = "Empty request counter",
             type = DataSourceType.COUNTER,
             description = "Total number of visits to the start page")
-    private final AtomicInteger emptyRequestCounter = new AtomicInteger(0);
+    private final AtomicInteger homePageRequestCounter = new AtomicInteger(0);
 
     @Monitor(name = "Factorial request counter",
             type = DataSourceType.COUNTER,
-            description = "Total number of visits to the start page")
+            description = "Total number of calculate factorial")
     private final AtomicInteger factorialRequestCounter = new AtomicInteger(0);
+
+    private final BasicTimer factorialDelayTimer = new BasicTimer(MonitorConfig.builder("factorialDelayTimer").build());
 
     @MonitorTags
     private final TagList tags = BasicTagList.of("id", "controller", "class", this.getClass().getSimpleName());
@@ -37,26 +46,42 @@ public class SimpleOneController {
     @PostConstruct
     public void init() {
         Monitors.registerObject("simpleOneController", this);
+        DefaultMonitorRegistry.getInstance().register(factorialDelayTimer);
     }
 
     @GetMapping
-    public String welcomePage() {
-        emptyRequestCounter.incrementAndGet();
+    public String homePage() {
+        homePageRequestCounter.incrementAndGet();
         return "Hello! This is service One";
     }
 
     @GetMapping("/factorial")
     public BigInteger getFactorialBy(@RequestParam("number") int number) {
         factorialRequestCounter.incrementAndGet();
-        BasicTimer timer = new BasicTimer(MonitorConfig.builder("timer").build());
-        Stopwatch stopwatch = timer.start();
+        Stopwatch stopwatch = factorialDelayTimer.start();
 
         BigInteger result = IntStream.rangeClosed(1, number)
                 .mapToObj(BigInteger::valueOf)
                 .reduce(BigInteger.ONE, BigInteger::multiply);
 
         stopwatch.stop();
-        System.out.println(timer.getValue());
+        factorialDelayTimer.record(stopwatch.getDuration(), TimeUnit.MILLISECONDS);
         return result;
+    }
+
+    @GetMapping("/factorial/simply")
+    public String getSimplyFactorialBy(@RequestParam("number") int number) {
+        factorialRequestCounter.incrementAndGet();
+        Stopwatch stopwatch = factorialDelayTimer.start();
+
+        BigInteger result = IntStream.rangeClosed(1, number)
+                .mapToObj(BigInteger::valueOf)
+                .reduce(BigInteger.ONE, BigInteger::multiply);
+
+        String resultAsString = simpleTwoClient.simplify(result);
+
+        stopwatch.stop();
+        factorialDelayTimer.record(stopwatch.getDuration(), TimeUnit.MILLISECONDS);
+        return resultAsString;
     }
 }
--- business-services/two/src/main/java/com/rntgroup/TwoApplication.java
+++ business-services/two/src/main/java/com/rntgroup/TwoApplication.java
@@ -3,15 +3,12 @@ package com.rntgroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
-public class OneApplication {
+public class TwoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OneApplication.class, args);
+        SpringApplication.run(TwoApplication.class, args);
     }
-
 }
